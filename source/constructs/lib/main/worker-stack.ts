54a3f3b8244951ec6ed77b2302c6dc9a81b53205
Qian <qianyanc@f84d8997c0be.ant.amazon.com>
2023-12-25 17:01:13 +0800

    update codebuild and api lambda

--- source/constructs/lib/main/worker-stack.ts
+++ source/constructs/lib/main/worker-stack.ts
@@ -36,15 +36,35 @@ export class WorkerStack extends Construct {
         buildSpec: codebuild.BuildSpec.fromObject({
           version: "0.2",
           phases: {
+            install: {
+              commands: [
+              'echo "git clone"',
+              'git clone --depth=1 --branch=${branch} ${code_commit_repo}',
+              'export DIR=$PWD',
+              'last_segment=$(echo "$code_commit_repo" | awk -F "/" "{print $NF}")',
+              'echo "$last_segment"',
+              'export TESTDIR=$DIR/"$last_segment"',
+              'echo $TESTDIR',
+              'cd $TESTDIR',
+              'echo "pip install"',
+              'echo $(python3 --version)',
+              'echo $(pip3 --version)',
+              'pip3 install -r requirements.txt'
+              ]
+            },
+
             build: {
               commands: [
-                'echo "Hello, CodeBuild!"', // Your build commands go here
+                'echo "start tests"', // Your build commands go here
+                'sh start_test_autotest_platform.sh "$mark" "$parameter" "$region"'
               ],
             },
           },
           post_build: {
             commands: [
-              `aws s3 sync testResult/ s3://${props.centralBucket.bucketName}/yourPrefix`, // Upload files from 'testResult' to S3
+              'current_time=$(date +"%H-%M-%S")',
+              'RESULTS_FILE_NAME="${project_name}-${mark}-${current_time}.json"',
+              `aws s3 cp test-report.json s3://${props.centralBucket.bucketName}/$project_name/$mark/$RESULTS_FILE_NAME`, // Upload files from 'testResult' to S3
             ],
           },
         }),
